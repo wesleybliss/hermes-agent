bbeda979b112ef9bb4c29177303971e586c99a26
Wesley Bliss <wesley.bliss@gmail.com>
2026-04-10 11:11:48 -0500

    Added bin directory for some setup scripts

---
+++ bin/setup-syncthing.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo useradd -r -s /bin/false -d /var/lib/syncthing syncthing
+sudo mkdir -p /var/lib/syncthing
+sudo chown -R syncthing:syncthing /var/lib/syncthing
+
+sudo systemctl disable syncthing@syncthing
+sudo systemctl start syncthing@syncthing
