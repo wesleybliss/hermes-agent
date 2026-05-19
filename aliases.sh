8d1192755e9eee28d584e0121a2b5220d8eb6dd1
Wesley Bliss <wesley.bliss@gmail.com>
2026-05-19 15:34:34 -0500

    Maintenance

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,5 @@
 
+alias hermes-upgrade='./bin/upgrade.sh'
 alias hermes-restart='(cd /root/docker && dcr -d)'
 alias hermes-bash='podman exec -it hermes bash'
 alias hermes-logs='podman logs -f hermes'
